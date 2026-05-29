44940131d76d0e08f488665482e9814833b598c5
Tom Brown <tom.brown@stud.hawk.de>
2026-05-29 18:44:17 +0200

    Added Sheet details to info box on PCB

--- EKG_Design/Ibom.bat
+++ EKG_Design/Ibom.bat
@@ -1,5 +1,23 @@
-:: Workaround für KiCad 10
-::Start in KiCad mit: set INTERACTIVE_HTML_BOM_NO_DISPLAY=1&& set PROJECTNAME=${PROJECTNAME}&& ${KIPRJMOD}\Ibom.bat
+@echo off
+setlocal
 
-::set
-"%ProgramFiles%\KiCad\10.0\bin\python.exe" %KICAD10_3RD_PARTY%plugins\org_openscopeproject_InteractiveHtmlBom\generate_interactive_bom.py"" --dest-dir export --name-format %%f --dark-mode --include-tracks --include-nets --no-browser --extra-fields manf# "%KIPRJMOD%\%PROJECTNAME%.kicad_pcb"
+cd /d "%~dp0"
+
+set "INTERACTIVE_HTML_BOM_NO_DISPLAY=1"
+set "PROJECTNAME=EKG_Design"
+set "KICAD_PY=C:\Program Files\KiCad\10.0\bin\python.exe"
+set "IBOM_PY=%KICAD10_3RD_PARTY%\plugins\org_openscopeproject_InteractiveHtmlBom\generate_interactive_bom.py"
+set "PCB_FILE=%~dp0%PROJECTNAME%.kicad_pcb"
+set "OUT_DIR=%~dp0bom"
+
+if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
+
+"%KICAD_PY%" "%IBOM_PY%" ^
+--dest-dir "%OUT_DIR%" ^
+--name-format ibom ^
+--dark-mode ^
+--no-browser ^
+--extra-fields "manf#" ^
+"%PCB_FILE%"
+
+endlocal
